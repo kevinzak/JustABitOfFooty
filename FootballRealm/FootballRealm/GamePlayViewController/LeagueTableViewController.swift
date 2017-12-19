0aa19050637236e175bf9a26d33820dcaa02beba
Kevin Zakszewski <KevinJZakszewski@gmail.com>
2017-12-19 15:33:24 -0500

    Reorganizing file structure

---
+++ FootballRealm/FootballRealm/GamePlayViewController/LeagueTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LeagueTableViewController.swift
+//  FootballRealm
+//
+//  Created by Kevin Zakszewski on 12/19/17.
+//  Copyright © 2017 Kevin Zakszewski. All rights reserved.
+//
+
+import UIKit
+
+class LeagueTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
